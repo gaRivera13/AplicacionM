55f0a24aff07cdf0717a73001c8de7e87574d5d9
Gabriela <ga.riverap@duocuc.cl>
2024-09-10 11:53:36 -0300

    arreglos p inicio

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -5,12 +5,19 @@ import { NivelEducacional } from 'src/app/model/nivel-educacional';
 import { Usuario } from 'src/app/model/usuario';
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.page.html',
-  styleUrls: ['./inicio.page.scss'],
+  selector: 'app-home',
+  templateUrl: 'inicio.page.html',
+  styleUrls: ['inicio.page.scss'],
 })
+
 export class InicioPage implements OnInit , AfterViewInit {
-  @ViewChild('titulo',({read: ElementRef}) itemTitulo : ElementRef);
+  @ViewChild('titulo', { read: ElementRef }) itemTitulo!: ElementRef;
+  @ViewChild('page', { read: ElementRef }) page!: ElementRef;
+  @ViewChild('itemCuenta', { read: ElementRef }) itemCuenta!: ElementRef;
+  @ViewChild('itemNombre', { read: ElementRef }) itemNombre!: ElementRef;
+  @ViewChild('itemApellido', { read: ElementRef }) itemApellido!: ElementRef;
+  @ViewChild('itemEducacion', { read: ElementRef }) itemEducacion!: ElementRef;
+  @ViewChild('itemFechaNacimiento', { read: ElementRef }) itemFechaNacimiento!: ElementRef;
 
   public usuario: Usuario = new Usuario('','','','','','','',
     NivelEducacional.findNivelEducacionalById(1)!,undefined);
@@ -23,7 +30,6 @@ export class InicioPage implements OnInit , AfterViewInit {
     private router: Router,
     private animationController: AnimationController
   )
-
   { 
     this.activatedRoute.queryParams.subscribe(params => { 
       const nav = this.router.getCurrentNavigation();
@@ -46,34 +52,75 @@ export class InicioPage implements OnInit , AfterViewInit {
   }
 
   public ngAfterViewInit(): void {
-    if(this.itemTitulo){
-      const animation = this.animationController
+    this.animationController
       .create()
       .addElement(this.itemTitulo.nativeElement)
       .iterations(Infinity)
       .duration(6000)
       .fromTo('transform','translate(0%)','translate(100%)')
-      .fromTo('opacity',0.2,1);
-    }
+      .fromTo('opacity',0.2 ,1)
+      .play();
   }
 
   public limpiar1(): void{
+    this.usuario.cuenta='';
+    this.usuario.nombre='';
+    this.usuario.apellido='';
+    this.usuario.nivelEducacional= NivelEducacional.findNivelEducacionalById(1)!;
+    this.usuario.fechaNacimiento=undefined;
+    this.animateItem1(this.itemCuenta.nativeElement, 800);
+    this.animateItem1(this.itemNombre.nativeElement, 1100);
+    this.animateItem1(this.itemApellido.nativeElement, 1400);
+    this.animateItem1(this.itemEducacion.nativeElement, 1700);
+    this.animateItem1(this.itemFechaNacimiento.nativeElement, 2000);
   }
 
   public limpiar2(): void{
+    this.usuario.cuenta='';
+    this.usuario.nombre='';
+    this.usuario.apellido='';
+    this.usuario.nivelEducacional= NivelEducacional.findNivelEducacionalById(1)!;
+    this.usuario.fechaNacimiento=undefined;
+    this.animateItem2(this.itemCuenta.nativeElement, 800);
+    this.animateItem2(this.itemNombre.nativeElement, 1100);
+    this.animateItem2(this.itemApellido.nativeElement, 1400);
+    this.animateItem2(this.itemEducacion.nativeElement, 1700);
+    this.animateItem2(this.itemFechaNacimiento.nativeElement, 2000);
   }
 
   public animateItem1(elementRef: any, duration:number){
+    this.animationController
+    .create()
+    .addElement(elementRef)
+    .iterations(1)
+    .duration(duration)
+    .fromTo('transform','translate(100%)','translate(0%)')
+    .play();
 
   }
   public animateItem2(elementRef: any, duration:number){
+    this.animationController
+    .create()
+    .addElement(elementRef)
+    .iterations(1)
+    .duration(duration)
+    .fromTo('transform','rotate(0deg)','rotate(360deg)')
+    .play();
   }
 
   createPageTurnAnimation(){
-    
+    this.animationController
+    .create()
+    .addElement(this.page.nativeElement)
+    .iterations(1)
+    .duration(1000)
+    .fromTo('transform','rotateY(0deg)','rotateY(-180deg)')
+    .duration(1000)
+    .fromTo('transform','rotateY(-180deg)','rotateY(0deg)')
+    .play();
   }
 
-  public mostratDatosPersona(): void{
+  public mostrarDatosPersona(): void{
     if (this.usuario.cuenta.trim() === ''){
       this.presentAlert('Datos personales', 'Para mostrar los datos de la persona, '
         + 'al menos debe tener un valor para el nombre o el apellido.');
@@ -88,12 +135,12 @@ export class InicioPage implements OnInit , AfterViewInit {
 
     let mensaje = `
       <small>
-        <br>Cuenta: ${this.usuario.cuenta}
-        <br>Usuario: ${this.usuario.correo}
-        <br>Nombre: ${this.usuario.nombre}
-        <br>Apellido: ${this.usuario.apellido}
-        <br>Educación: ${this.usuario.getTextoNivelEducacional()}
-        <br>Nacimiento: ${this.formatDateDDMMYYYY(this.usuario.fechaNacimiento)}
+        <br><b>Cuenta:</b> ${this.usuario.cuenta}
+        <br><b>Usuario:</b> ${this.usuario.correo}
+        <br><b>Nombre:</b> ${this.usuario.nombre}
+        <br><b>Apellido:</b> ${this.usuario.apellido}
+        <br><b>Educación:</b> ${this.usuario.getTextoNivelEducacional()}
+        <br><b>Nacimiento:</b> ${this.formatDateDDMMYYYY(this.usuario.fechaNacimiento)}
       </small>
     `;
     this.presentAlert('Datos personales', mensaje);
